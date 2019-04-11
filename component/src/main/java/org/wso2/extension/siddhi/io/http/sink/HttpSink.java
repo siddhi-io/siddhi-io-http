24551507889c66d7659e3ccf2a97407943b3b247
mohan <mohan.vive@gmail.com>
2019-04-11 16:51:30 +0530

    Update Siddhi dependency and fix for API change

--- component/src/main/java/org/wso2/extension/siddhi/io/http/sink/HttpSink.java
+++ component/src/main/java/org/wso2/extension/siddhi/io/http/sink/HttpSink.java
@@ -639,7 +639,7 @@ public class HttpSink extends Sink {
     }
 
     @Override
-    protected ServiceDeploymentInfo exposedServiceDeploymentInfo() {
+    protected ServiceDeploymentInfo exposeServiceDeploymentInfo() {
         return null;
     }
 
