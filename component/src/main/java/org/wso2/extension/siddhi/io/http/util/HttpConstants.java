cd4ec68e1e37ba3eb49b27900aaf9c1a26db4a4c
dnwick <dnsnipe123@gmail.com>
2019-06-07 12:42:37 +0530

    Add support to disable hostname verification

--- component/src/main/java/org/wso2/extension/siddhi/io/http/util/HttpConstants.java
+++ component/src/main/java/org/wso2/extension/siddhi/io/http/util/HttpConstants.java
@@ -238,4 +238,5 @@ public class HttpConstants {
     public static final String ACCESS_TOKEN = "access_token";
     public static final String REFRESH_TOKEN = "refresh_token";
     public static final String BLOCKING_IO = "blocking.io";
+    public static final String HOSTNAME_VERIFICATION_ENABLED = "hostname.verification.enabled";
 }
